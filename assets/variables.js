9f7d7a48cff9f780849e22554ce5add99625a1a2
freiza24 <kaniasafitri690@gmail.com>
2024-09-10 20:31:23 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Intan", "Tann", "Intunnn", "Intannnn"],
+  nicknames: ["Salza", "Sasa", "Cengos", "Sa"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
